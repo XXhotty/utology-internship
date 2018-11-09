6656582d97573560c1a91447de36126832c1141e
= <=>
2018-11-09 07:36:27 +0000

    1

--- kadai1-4-2.php
+++ kadai1-4-2.php
@@ -4,9 +4,9 @@ $fp = fopen("file-sample.txt", "r") or die("ファイルが開けないよぉ！
  
 // ファイルへ書き込み
 fwrite($fp, "==== ==== ==== ==== ====\n");
-fwrite($fp,$_POST["onamae"] ."");
+fwrite($fp,"$_POST["onamae"] .""");
 fwrite($fp, "==== ==== ==== ==== ====\n");
-
+-
 // 読み込んだファイルを出力
 while( !feof($fp) ) {
   echo fgets($fp);
