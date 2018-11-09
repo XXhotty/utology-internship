d9d96e9501c02659267128becf3867c1b42224cd
= <=>
2018-11-09 05:24:14 +0000

    1

---
+++ kadai1-3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>フォームからPOSTで送信されたデータを表示 php検索</title>
+</head>
+<body>
+<form method="POST" action="kadai1-3.php">
+
+<?php
+if (strpos($subject,'php') !== false) {
+	 echo $_POST["onamae"] ."";
+    }
+?>
+
+<br />
+<label>文字列を入力してください：</label>
+<input type="text" name="onamae" /><br />
+<input type="submit" value="送信" />
+</form>
+</body>
+</html>
