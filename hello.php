53b05b0ed436abe00e0b6b05497b5707e6c4381a
= <=>
2018-11-15 23:17:21 +0000

    1

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+ // smarty ‚Ìƒ‰ƒCƒuƒ‰ƒŠ‚ð“Ç‚Ýž‚Ý‚Ü‚·
+ include_once __DIR__ . '/libs/smarty.class.php';
+ 
+ // smarty‚ðéŒ¾‚µ‚ÄÝ’è‚ð‘‚«‰Á‚¦‚Ü‚·
+ $smarty = new Smarty();
+ $smarty->escape_html = true;
+ $smarty->template_dir = __DIR__ . '/templates';
+ $smarty->compile_dir = __DIR__ . '/templates_c';
+ 
+ $name = '“c’†';
+ $smarty->assign('name', $name);
+ $smarty->display('hello.tpl');
