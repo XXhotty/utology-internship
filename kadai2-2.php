7c45020fdafb4f7c2a26bf340484128acbe92aef
= <=>
2018-11-24 05:59:28 +0000

    1

--- kadai2-2.php
+++ kadai2-2.php
@@ -7,7 +7,7 @@ $DBPASSWD = 'pw'; //作成したユーザーのパスワード
 $dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8';
 $pdo = new \PDO($dsn, $DBUSER, $DBPASSWD, array(\PDO::ATTR_EMULATE_PREPARES => false));
 
-if ( $pdo !== null ) {
+if ( $pdo !== false ) {
 
     $msg     = '';
     $err_msg = '';
