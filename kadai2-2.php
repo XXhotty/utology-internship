9742341e1d5c167bd72fff8dfd97b399680c748b
= <=>
2018-12-07 01:52:43 +0000

    1

--- kadai2-2.php
+++ kadai2-2.php
@@ -2,8 +2,8 @@
 
 $DBSERVER = 'localhost';
 $DBNAME = 'board';
-$DBUSER = 'board'; //作成したユーザー名
-$DBPASSWD = 'pw'; //作成したユーザーのパスワード
+$DBUSER = 'hotty'; //作成したユーザー名
+$DBPASSWD = 'hotta'; //作成したユーザーのパスワード
 $dsn = 'mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8';
 $pdo = new \PDO($dsn, $DBUSER, $DBPASSWD, array(\PDO::ATTR_EMULATE_PREPARES => false));
 
