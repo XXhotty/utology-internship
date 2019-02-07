ce4a0ac6a8bc96a09a40d9557a77b23649911eb9
XXhotty <fruitsnomori1026@gmail.com>
2019-02-07 10:36:57 +0900

    1

--- 3-9.php
+++ 3-9.php
@@ -49,25 +49,13 @@ catch(PDOException $e){
 
 // smarty のライブラリを読み込みます
 include_once __DIR__ . '/libs/Smarty.class.php';
-?>
+
 // smartyを宣言して設定を書き加えます
-<!DOCTYPE HTML>
+$smarty = new Smarty();
+$smarty->escape_html = true;
+$smarty->template_dir = __DIR__ . '/templates';
+$smarty->compile_dir = __DIR__ . '/templates_c';
 
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>media</title>
-</head>
+$smarty->display('3-9.tpl');
 
-<body>
-<form action="3-9.php" enctype="multipart/form-data" method="post">
-    <label>動画アップロード</label>
-    <input type="file" name="upfile">
-    <br>
-※画像はjpeg方式，png方式，gif方式に対応しています．動画はmp4方式のみ対応しています．<br>
-    <input type="submit" value="アップロード">
-</form>
-
-</body>
-</html>
 
