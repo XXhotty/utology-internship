c9c21b3f1af80fbedc719d81d7d3697459ba85e9
XXhotty <fruitsnomori1026@gmail.com>
2019-02-07 11:36:10 +0900

    1

--- 3-9.php
+++ 3-9.php
@@ -34,6 +34,7 @@ try {
     echo("<br/>");
     echo("a.<br/>");
 
+
     if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
         if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "files/" . $_FILES["upfile"]["name"])) {
             chmod("files/" . $_FILES["upfile"]["name"], 0644);
