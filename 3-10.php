9159bd535d3f64f1f0090c76ab659f713c1ec97e
XXhotty <fruitsnomori1026@gmail.com>
2019-02-10 11:21:22 +0900

    1

--- 3-10.php
+++ 3-10.php
@@ -4,11 +4,24 @@
     <title>sample</title>
 </head>
 <body>
-<form action="3-9.php" method="post" enctype="multipart/form-data">
-    ファイル：<br />
-    <input type="file" name="upfile" size="30" /><br />
-    <br />
-    <input type="submit" value="アップロード" />
+<form method="POST" action="">
+
+
+    <?php
+    $sql = "SELECT * FROM mp4 ORDER BY id;";
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute();
+    while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)){
+        echo ($row["id"]."<br/>");
+        $target = "files/".$row["name"];
+        echo ($target."<br/>");
+
+        echo ("<video id =video src=\"$target\" width=\"426\" height=\"240\" controls></video>");
+        }
+    echo ("<br/><br/>");
+
+?>
+
 </form>
 </body>
 </html>
