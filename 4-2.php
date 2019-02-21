225aaf8af42b7019225b9c5563f873018cd61c37
XXhotty <fruitsnomori1026@gmail.com>
2019-02-21 22:06:07 +0900

    1

--- 4-2.php
+++ 4-2.php
@@ -1,19 +1,8 @@
 <?php
-echo("4-2started.<br/>");
-include_once __DIR__ . '/libs/Smarty.class.php';
-$smarty = new Smarty();
-$smarty->escape_html = true;
-$smarty->template_dir = __DIR__ . '/templates';
-$smarty->compile_dir = __DIR__ . '/templates_c';
 
 require_once __DIR__ . '/libs/dao/AccessDao.php';
 $db = new DB(localhost,hotty,hotta,board);
 $sql = "SELECT * FROM mp4 ORDER BY id;";
 $result = $db->fetch($sql);
-
-echo("$result.<br/>");
-echo("$data.<br/>");
-
-$smarty->assign('data', $data);
-$smarty->display('4-2.tpl');
+print_r($result);
 
