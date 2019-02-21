9056c1cba2398a82a1851533d8d4bb8a48e54c75
XXhotty <fruitsnomori1026@gmail.com>
2019-02-21 16:15:10 +0900

    1

--- 4-1.php
+++ 4-1.php
@@ -17,39 +17,44 @@ $DBUSER = "hotty";
 $DBPASSWD = "hotta";
 $DBNAME = "board";
 
-$name = $_FILES["upfile"]["name"];
-$title     = $_POST['title'];
-$tmp = pathinfo($_FILES["upfile"]["name"]);
-$extension = $tmp["extension"];
-
-
-$create = new create($name, $title);
 $hoge = 0;
+
 echo("$hoge.<br/>");
 $hoge++;
 
-if ($name !== '' && is_uploaded_file($_FILES["upfile"]["tmp_name"])){
-    if ($extension == "mp4") {
-        if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "files/" . $_FILES["upfile"]["name"])) {
-            chmod("files/" . $_FILES["upfile"]["name"], 0644);
-            $messages =$_FILES["upfile"]["name"] . "をアップロードしました。";
-            echo("$hoge.<br/>");
-            $hoge++;
+if (isset($_FILES['upfile']['error'])) {
 
-            $create->execute();
+    $name = $_FILES["upfile"]["name"];
+    $title = $_POST['title'];
+    $tmp = pathinfo($_FILES["upfile"]["name"]);
+    $extension = $tmp["extension"];
 
+    $create = new create($name, $title);
+
+    echo("$hoge.<br/>");
+    $hoge++;
+
+    if ($name !== '' && is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
+        if ($extension == "mp4") {
+            if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "files/" . $_FILES["upfile"]["name"])) {
+                chmod("files/" . $_FILES["upfile"]["name"], 0644);
+                $messages = $_FILES["upfile"]["name"] . "をアップロードしました。";
+                echo("$hoge.<br/>");
+                $hoge++;
+
+                $create->execute();
+
+            } else {
+                $messages = "ファイルをアップロードできません。";
+            }
+        } else {
+            $messages = "非対応ファイルです";
         }
-        else {
-            $messages = "ファイルをアップロードできません。";
-        }
-    }
-    else {
-        $messages ="非対応ファイルです";
+    } else {
+        $messages = "タイトルが入力されていない、もしくはファイルが選択されていません。";
     }
 }
-else {
-    $messages = "タイトルが入力されていない、もしくはファイルが選択されていません。";
-}
+
 echo("$hoge.<br/>");
 $hoge++;
 
