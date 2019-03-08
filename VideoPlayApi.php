2b075d4ee54a676095cc82158f06235c60b884ac
XXhotty <fruitsnomori1026@gmail.com>
2019-03-08 14:48:52 +0900

    1

--- VideoPlayApi.php
+++ VideoPlayApi.php
@@ -9,10 +9,10 @@ $post_data_3 = $_POST['post_data_3'];
 
 if ( $post_data_1 !== '' && $post_data_2 !== ''&& $post_data_3 !== '' ) {
     $result = $db->comment($post_data_1, $post_data_2, $post_data_3);
-    $return_array = array("PHPに送られたpost_data_1:".$post_data_1, "PHPに送られたpost_data_2:".$post_data_2, "PHPに送られたpost_data_3:".$post_data_3);
+    $return_array = array($post_data_1,$post_data_2,$post_data_3);
 }
 else{
-    $return_array = array("mysqlに保存失敗しました。PHPに送られたpost_data_1:".$post_data_1, "PHPに送られたpost_data_2:".$post_data_2, "PHPに送られたpost_data_3:".$post_data_3);
+    $return_array = array($post_data_1,$post_data_2,$post_data_3);
 }
 
 header('Content-type:application/json; charset=utf8');
