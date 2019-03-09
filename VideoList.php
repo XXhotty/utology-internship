a303c5f29c9bda7a6b60b1e402b9d1e30b9a0e09
XXhotty <fruitsnomori1026@gmail.com>
2019-03-09 15:12:05 +0900

    1

--- VideoList.php
+++ VideoList.php
@@ -10,7 +10,7 @@ $db = new DB(localhost,hotty,hotta,board);
 
 if(isset($_POST["word"])){
     $word = $_POST["word"];
-    $sql = "SELECT * FROM mp4 WHERE title LIKE N'%word%';";
+    $sql = "SELECT * FROM mp4 WHERE title LIKE N'%$word%';";
     $result = $db->fetch($sql);
     var_dump($result);
 }else{
